94b0993483e02b1945fd7190d09a602ad168d396
WENJIE WANG <wenjiew@student.unimelb.edu.au>
2019-09-25 14:38:53 +1000

    wenjie's third modification

--- memory_app/app/src/main/java/com/example/mem_app/ViewAllItemsActivity.java
+++ memory_app/app/src/main/java/com/example/mem_app/ViewAllItemsActivity.java
@@ -2,6 +2,7 @@ package com.example.mem_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,4 +15,15 @@ public class ViewAllItemsActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
     }
+
+    public void onLogoutButtonClick(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void onHomeButtonClick(View view) {
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+
 }
