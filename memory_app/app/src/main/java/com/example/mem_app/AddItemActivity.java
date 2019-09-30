18480a0e2ddcb4316f23e5aae8733b2a9661c598
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-01 02:47:07 +1000

    finishing upload view and myinfo

---
+++ memory_app/app/src/main/java/com/example/mem_app/AddItemActivity.java
@@ -0,0 +1,158 @@
+package com.example.mem_app;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.app.AlertDialog;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+
+public class AddItemActivity extends AppCompatActivity{
+
+    private ImageView item_image;
+    private String image_string;
+    public static Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show add_item page
+        setContentView(R.layout.add_item);
+        Intent intent = getIntent();
+        context = getApplicationContext();
+    }
+
+    public void onAddButtonClick(View view){
+        final TextView text_item_name = (TextView) super.findViewById(R.id.ItemName);
+        final TextView text_description = (TextView) super.findViewById(R.id.Description);
+        final String text_image_string = image_string;
+        final String user_name = MainActivity.user_profile.user_name;
+        final String user_pwd = MainActivity.user_profile.user_pwd;
+
+        System.out.println("USER_NAME = " + user_name);
+        System.out.println("USER_PWD = " + user_pwd);
+        System.out.println("Image String = " + text_image_string);
+
+        String resp = MainActivity.processor.addItemHttpSend(   text_item_name,
+                                                                text_description,
+                                                                text_image_string,
+                                                                user_name,
+                                                                user_pwd);
+        if (resp.equals("###ADD_ITEM_SUCCESS###")) {
+            final CharSequence[] options = { "OK" };
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddItemActivity.this);
+            builder.setTitle("Success!");
+            builder.setItems(options, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("OK")) {
+                    dialog.dismiss();
+                    Intent i = new Intent(context, HomeActivity.class);
+                    startActivity(i);
+                }
+                }
+            });
+            builder.show();
+        }
+        else if (resp.equals("###ADD_ITEM_FAILED###")){
+            final CharSequence[] options = { "Cancel" };
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddItemActivity.this);
+            builder.setTitle("Failure!");
+            builder.setItems(options, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+                }
+            });
+            builder.show();
+        }
+
+    }
+    public void onLogoutButtonClick(View view) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void onHomeButtonClick(View view) {
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+
+
+    public void onUploadButtonClick(View view) {
+        final CharSequence[] options = { "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddItemActivity.this);
+        builder.setTitle("Choose Your Photo");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+                BitMapToString(bitmap);
+                item_image= findViewById(R.id.ImageView);
+                item_image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String BitMapToString(Bitmap userImage1) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        userImage1.compress(Bitmap.CompressFormat.PNG, 60, baos);
+        byte[] b = baos.toByteArray();
+        image_string = Base64.encodeToString(b, Base64.DEFAULT);
+        return image_string;
+    }
+
+    private void popAlert(String text){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setPositiveButton("ok",null);
+        AlertDialog ad = alertDialogBuilder.create();
+        ad.setMessage(text);
+        //alertDialogBuilder.setPositiveButton("ok",null);
+        ad.show();
+    }
+
+
+
+}
---
+++ memory_app/app/src/main/java/com/example/mem_app/SignUpActivity.java
@@ -0,0 +1,69 @@
+package com.example.mem_app;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show login page
+        setContentView(R.layout.sign_up);
+    }
+
+    public void onGoBackButtonClick(View view) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void onSignUpButtonClick(View view) {
+        final TextView text_username = (TextView) super.findViewById(R.id.signup_text_username);
+        final TextView text_password = (TextView) super.findViewById(R.id.signup_text_password);
+        final TextView text_first_name = (TextView) super.findViewById(R.id.signup_text_first_name);
+        final TextView text_last_name = (TextView) super.findViewById(R.id.signup_text_last_name);
+        final TextView text_email = (TextView) super.findViewById(R.id.signup_text_email);
+        final TextView text_dob = (TextView) super.findViewById(R.id.signup_text_dob);
+
+        String resp = MainActivity.processor.signupHttpSend(text_username, text_password,
+                text_first_name, text_last_name, text_email, text_dob);
+
+        // test flask's response
+        if (resp.equals("###USER_REG_SUCCESS###")) {
+            popAlert("user register success");
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+
+
+        } else if (resp.equals("###NON_VALID_EMAIL###")) {
+            popAlert("Non valid email.\nTry again.");
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+
+        } else if (resp.equals("###USER_REG_FAILED_2###")) {
+            popAlert("Please fulfill your username, password and email.\nTry again.");
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+
+        } else if (resp.equals("###USER_REG_FAILED###")) {
+            popAlert("User name already in use.\nTry again.");
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+
+        }
+    }
+
+    private void popAlert(String text){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setPositiveButton("ok",null);
+            AlertDialog ad = alertDialogBuilder.create();
+            ad.setMessage(text);
+            //alertDialogBuilder.setPositiveButton("ok",null);
+            ad.show();
+    }
+}
