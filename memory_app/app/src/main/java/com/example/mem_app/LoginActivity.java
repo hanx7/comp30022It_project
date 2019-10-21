deefb2e1185450e3f6700dd122a09bda95259be9
Chuying Yue <chuyingy@student.unimelb.edu.au>
2019-10-22 05:49:28 +1100

    ui2

--- memory_app/app/src/main/java/com/example/mem_app/LoginActivity.java
+++ memory_app/app/src/main/java/com/example/mem_app/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Show login page
-        setContentView(R.layout.edit_event);
+        setContentView(R.layout.login);
     }
 
     public void onLoginButtonClick(View view) {
